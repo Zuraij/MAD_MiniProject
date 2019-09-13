a1456b894fe44921fd77a3aa400f9252a5b48815
Pirathikaran <48381388+Pirathikaran@users.noreply.github.com>
2019-09-13 19:49:58 +0530

    Add files via upload

--- app/src/main/java/com/example/mad_miniproject/MainActivity.java
+++ app/src/main/java/com/example/mad_miniproject/MainActivity.java
@@ -1,11 +1,7 @@
-package com.example.mad_miniproject;
+package com.example.user.mad_new;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,23 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button1;
-        button1 = (Button) findViewById(R.id.addButton);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openAddNotice();
-            }
-        });
-
     }
-
-    private void openAddNotice() {
-
-        Intent intent = new Intent(this, AddNotice.class);
-        startActivity(intent);
-    }
-
-
 }
